125f7fdfc611b33995bc423dfa0ca6e7ca6de574
shilaranidas <shilarani.das@georgebrown.ca>
2021-02-07 00:39:51 -0500

    add operator overloading output stream

--- Game1011_Lab2_Das/Weapon.h
+++ Game1011_Lab2_Das/Weapon.h
@@ -21,13 +21,22 @@ public:
 		}
 		return "failed to add ability";
 	}
+	void getAllAbilities()
+	{
+		for (int i = 0; i < current_no_of_ability; i++)
+		{
+			
+		}
+	}
 	ostream& operator<<(ostream& out)
 	{
 		out <<"Weapon name:"<< this->m_name<<"; description: "<<this->m_description<<"; damage value: "<<this->m_damageValue<<endl;
-		/*for each (string var in m_abilities)
+		out << "Ablities: ";
+		for (int i = 0; i < current_no_of_ability-1; i++)
 		{
-
-		}*/
+			out << this->m_abilities[i] << ", ";
+		}
+		out << this->m_abilities[current_no_of_ability - 1]<<endl;
 		return out;
 	}
 };
