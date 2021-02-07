ffeb2d2b205844c4955b5211918a25011c833aee
shilaranidas <shilarani.das@georgebrown.ca>
2021-02-07 18:20:42 -0500

    tested successfully

--- Game1011_Lab2_Das/Game1011_Lab2_Das.cpp
+++ Game1011_Lab2_Das/Game1011_Lab2_Das.cpp
@@ -42,10 +42,11 @@ int main()
 		{
 		case 'a':
 			std::cout << "-------------------------------------------------\n";
-			std::cout << "\tPlease select the type of your player :" << "\n";
-			std::cout << "\t1. Human (has health=10, ability=hard, strong";
-			std::cout << "\n\t2. Orcs(has health=15, ability=)";
-			std::cout << "\n\t3.Undead"<< endl;
+			std::cout << "\tPlease select the type of your player :";
+			std::cout << "\n\t1. Human (has health=10, ability=hard, strong";
+			std::cout << "\n\t2. Orcs(has health=15, ability=fast, decent, range, roll-catching)";
+			std::cout << "\n\t3. Undead(has health=20, ability=damage,rockfire,return)"<< endl;
+			
 			std::cout << "-------------------------------------------------\n";
 			std::cout<<"Player type: ";
 			std::cin >> charType;
@@ -61,21 +62,27 @@ int main()
 			case 1:
 				ch[charIndex] = new Humans(name);
 				charIndex++;
-				cout << "Added character!" << endl;
+				cout << "Added Human character!" << endl;
 				break;
 			case 2:
+				ch[charIndex] = new Orcs(name);
+				charIndex++;
+				cout << "Added Orcs character!" << endl;
 				break;
 			case 3:
+				ch[charIndex] = new Undead(name);
+				charIndex++;
+				cout << "Added undead character!" << endl;
 				break;
 			
 			}
 			break;
 		case 'b':
 			std::cout << "-------------------------------------------------\n";
-			std::cout << "All type of characters:\n";
+			std::cout << "All type of characters:"<<endl;
 			for (int i = 0; i < charIndex; i++)
 			{
-				cout << "ID "<<i<<endl;
+				cout << "--------------------ID: "<<i+1<<"----------------------------";
 				ch[i]->DisplayInfo();
 			}
 			break;
@@ -85,6 +92,7 @@ int main()
 			cin >> deleteIndex;
 			std::cin.ignore(INT_MAX, '\n'); // Clear/flush out anything left in buffer.
 										   // If we ARE in a fail state, this line will get ignored.
+			deleteIndex--;
 			if (charIndex > deleteIndex)
 			{
 				for (int i = deleteIndex; i < charIndex; i++)
@@ -96,6 +104,10 @@ int main()
 				//delete ch[deleteIndex];
 				//ch[deleteIndex] = nullptr;
 				cout << "Deleted successfully!" << endl;
+			}
+			else
+			{
+				cout << "Deleted failed for out of index!" << endl;
 			}
 			
 			break;
