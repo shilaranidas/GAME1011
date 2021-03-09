94b80537a0966073335c1c2e9f453ec5d492ae87
shilaranidas <shilarani.das@georgebrown.ca>
2021-03-09 01:18:18 -0500

    add sort function

--- Game1011_Lab3_Das/SortableVector.h
+++ Game1011_Lab3_Das/SortableVector.h
@@ -8,7 +8,7 @@ public:
     SortableVector(int s) :SimpleVector<T>(s) {}//constructor
     SortableVector(SortableVector& obj) : SimpleVector<T>(obj) {};//copy constructor
     SortableVector(SimpleVector<T>& obj) : SimpleVector<T>(obj) {}//additional copy constructor
-   
+    void sort();//sort the arrayin ascending order.
 
 };
 /*
@@ -19,4 +19,23 @@ SearchableVector<T>::SearchableVector(SearchableVector& obj) :SimpleVector<T>(ob
 
 }*/
 
+template<class T>
+void SortableVector<T>::sort()
+{
+    int i, j;
+    T key;
+    //insertion sort
+    for (i = 1; i < this->size(); i++) {
+        key = this->operator[](i);
+        j = i - 1;
 
+        /* Move elements of arr[0..i-1], that are
+          greater than key, to one position ahead
+          of their current position */
+        while (j >= 0 && this->operator[](j) > key) {
+            this->operator[](j + 1) = this->operator[](j);
+            j = j - 1;
+        }
+        this->operator[](j + 1) = key;
+    }
+}
